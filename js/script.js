287666a411629e57e5b69b1847e69bc73a7ddd84
Alexandru Davidenco <ozziaqueen@gmail.com>
2024-06-16 15:09:50 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//alert("Hello");
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18 лет?", "");
+//console.log(typeof(answer));
+
+const answers = [];
+answers[0] = prompt("Как ваше имя?");
+answers[1] = prompt("Как ваша фамилия?");
+answers[2] = prompt("Сколько вам лет?");
+
+console.log(typeof(answers));
